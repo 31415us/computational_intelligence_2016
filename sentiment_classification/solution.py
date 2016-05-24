77771703d40866f4a6e31f0ff14f74e8e362dbd8
Michel Kaporin <michel.kaporin@gmail.com>
2016-05-24 23:38:15 +0200

    SVM Classifier added. Stop word filter removed.

--- sentiment_classification/solution.py
+++ sentiment_classification/solution.py
@@ -1,12 +1,10 @@
 
-import re
-import nltk
+from sklearn import svm
 import numpy as np
 import pickle
 
 # initialise globals
 featureList = []
-stop_words = []
 word_to_id = {}
 
 #start parse_vocabulary
@@ -20,30 +18,13 @@ def parse_vocabulary(file):
         count += 1
 #end
 
-#start get_stopword_list
-def get_stopword_list(stop_words_file):
-    #read the stop_words file and build a list
-    stop_words = []
-    stop_words.append('<user>')
-    stop_words.append('<url>')
-
-    fp = open(stop_words_file, 'r')
-    line = fp.readline()
-    while line:
-        word = line.strip()
-        stop_words.append(word)
-        line = fp.readline()
-    fp.close()
-    return stop_words
-#end
-
 #start get_word_vector
 def get_word_vector(word, embeddings):
     # Returns word vector from generated embeddings
     word_vector = np.array([])
     
     # If word exists in word_to_id dictionary, then grab the corresponding embeddings vector
-    if word in word_to_id and word not in stop_words:
+    if word in word_to_id:
         embd_index = word_to_id[word]
         word_vector = embeddings[embd_index,:]
         
@@ -76,17 +57,17 @@ def extract_features(tweet, embeddings):
 #end
 
 #start print_classified
-def print_classified(max_ent_classifier, embeddings):
+def print_classified(classifier, embeddings):
     # Extract all tweets to classify
     testTweet = "<user> yep gonna watch it now"
     testTweet1 = "i'm glad this day is ending"
     testTweet2 = "why is she so perfect <url>"
     testTweet3 = "why do i get treated like this"
     
-    print(max_ent_classifier.classify(extract_features(testTweet, embeddings)))
-    print(max_ent_classifier.classify(extract_features(testTweet1, embeddings)))
-    print(max_ent_classifier.classify(extract_features(testTweet2, embeddings)))
-    print(max_ent_classifier.classify(extract_features(testTweet3, embeddings)))
+    print(classifier.predict(extract_features(testTweet, embeddings)))
+    print(classifier.predict(extract_features(testTweet1, embeddings)))
+    print(classifier.predict(extract_features(testTweet2, embeddings)))
+    print(classifier.predict(extract_features(testTweet3, embeddings)))
 #end
 
 #start cross_validate
@@ -103,39 +84,34 @@ def main():
     
     embeddings = np.load('embeddings.npy')
     
-    #Read the tweets one by one and process it
-    stop_words = get_stopword_list('stopwords.txt')
+    #Read the tweets and process them
+    tweets = []
     pos_tweets = open('train_pos_cut.txt', 'rb')
     neg_tweets = open('train_neg_cut.txt', 'rb')
     
-    # Get tweets, label them
-    tweets = []
+    # Preparing feature set for classifier
+    training_set = []
+    label_set = []
+    
     for row in neg_tweets:
         sentiment = 'negative'
         tweet = row.decode('utf-8')
-        tweets.append((tweet, sentiment));
+        features = np.array((extract_features(tweet, embeddings))).tolist()
+        training_set.append(features)
+        label_set.append(sentiment)
     for row in pos_tweets:
         sentiment = 'positive'
         tweet = row.decode('utf-8')
-        tweets.append((tweet, sentiment));
+        features = np.array((extract_features(tweet, embeddings))).tolist()
+        training_set.append(features)
+        label_set.append(sentiment)
     #end loops
-        
-    # Preparing feature set for classifier
-    training_set = []
-    for tw in tweets:
-        features = np.array((extract_features(tw[0], embeddings))).tolist()
-        training_set.append((features, tw[1]))
     
-    print(training_set[0])
-    #print(training_set[0][1])
-    # Max Entropy Classifier
-    max_ent_classifier = nltk.classify.maxent.MaxentClassifier.train(training_set, 'GIS', trace=3, \
-                        encoding=None, labels=None, gaussian_prior_sigma=0, max_iter = 3)
-                        
-    print_classified(max_ent_classifier, embeddings)
-
-    #print informative features
-    print(max_ent_classifier.show_most_informative_features(10))
+    # Train classifier
+    classifier = svm.SVC()
+    classifier.fit(training_set, label_set)
+    
+    print_classified(classifier, embeddings)
     
     
 if __name__ == '__main__':
