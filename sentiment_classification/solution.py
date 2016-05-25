c497944318dfd583eb46d13248c95bac0fa98e3b
Teymur Babayev <tima.babayev@gmail.com>
2016-05-25 15:21:58 +0200

    Slightly optimized

--- sentiment_classification/solution.py
+++ sentiment_classification/solution.py
@@ -3,7 +3,6 @@ from sklearn import cross_validation
 
 # Tf-Idf
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
 
 import numpy as np
 import pickle
@@ -16,7 +15,7 @@ word_to_id = {}
 def parse_vocabulary(file):
     with open(file, 'rb') as f:
         vocab = pickle.load(f)
-    
+
     count = 0
     for w in vocab:
         word_to_id[w] = count
@@ -42,43 +41,43 @@ def read_training_set(pos_tweets, neg_tweets, embeddings):
             features = np.array((extract_features(tweet, embeddings))).tolist()
             training_set.append(features)
             labels.append('positive')
-            
+
     return training_set, labels
 
 #start get_word_vector
 def get_word_vector(word, embeddings):
     # Returns word vector from generated embeddings
     word_vector = np.array([])
-    
+
     # If word exists in word_to_id dictionary, then grab the corresponding embeddings vector
     if word in word_to_id:
         embd_index = word_to_id[word]
         word_vector = embeddings[embd_index,:]
-        
+
     return word_vector
 #end
 
 #start extract_features
 def extract_features(tweet, embeddings):
     tweet_words = set(tweet)
-    
+
     features = np.array(embeddings.shape[0])
     word_count = 0
     for word in tweet_words:
         # get word from embeddings
         feature = get_word_vector(word, embeddings)
-        
+
         if feature.size is 0: # If feature is empty, skip iteration
             continue
-            
+
         # sum with others
         features = np.add(features, feature)
         # increase the count
         word_count += 1
-        
+
     # divide by the word_count
     np.divide(features, word_count)
-    
+
     return features
 #end
 
@@ -89,7 +88,7 @@ def print_classified(classifier, embeddings):
     testTweet1 = "i'm glad this day is ending"
     testTweet2 = "why is she so perfect <url>"
     testTweet3 = "why do i get treated like this"
-    
+
     print(classifier.predict(extract_features(testTweet, embeddings)))
     print(classifier.predict(extract_features(testTweet1, embeddings)))
     print(classifier.predict(extract_features(testTweet2, embeddings)))
@@ -117,37 +116,37 @@ def cross_validate(classifier, features_test, labels_test):
 #end
 
 def main():
-    word_to_id = parse_vocabulary('vocab.pkl')  
+    word_to_id = parse_vocabulary('vocab.pkl')
     embeddings = np.load('embeddings.npy')
-    
+
     # Read the tweets and prepare them for classifier
     pos_tweets = open('train_pos_cut.txt', 'r')
     neg_tweets = open('train_neg_cut.txt', 'r')
     training_set, labels = read_training_set(pos_tweets, neg_tweets, embeddings)
-      
+
     if tf_idf: # If Tf-Idf is used, vectorize the data we have
         vectorizer = TfidfVectorizer()
         training_set = vectorizer.fit_transform(training_set)
-    
+
     # Split dataset for cross_validation
     training_set, test_set, training_set_labels, test_set_labels = cross_validation.train_test_split(
         training_set, labels, test_size=0.4, random_state=0)
-    
+
     ###
     # Train SVM classifier & print the results
     ###
     classifier = svm.SVC()
     classifier.fit(training_set, training_set_labels)
-    
-    print_classified_with_tfidf(classifier, vectorizer) if tf_idf else print_classified(classifier, embeddings)   
+
+    print_classified_with_tfidf(classifier, vectorizer) if tf_idf else print_classified(classifier, embeddings)
     print("Accuracy of SVM %s" % cross_validate(classifier, test_set, test_set_labels))
-    
+
     ###
     # Traing logistic regression classifier  & print the results
     ###
     classifier = linear_model.LogisticRegression()
     classifier.fit(training_set, training_set_labels)
-    
+
     print_classified_with_tfidf(classifier, vectorizer) if tf_idf else print_classified(classifier, embeddings)
     print("Accuracy of log regression: %s" % cross_validate(classifier, test_set, test_set_labels))
 
