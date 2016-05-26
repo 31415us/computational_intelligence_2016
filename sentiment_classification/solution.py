fcb99b5e5e687f9faa53e8c8cd7bdc4594eed74b
Teymur Babayev <tima.babayev@gmail.com>
2016-05-26 13:47:46 +0200

    Code beautified, linear svc change made

--- sentiment_classification/solution.py
+++ sentiment_classification/solution.py
@@ -1,7 +1,5 @@
 from sklearn import svm, linear_model
 from sklearn.cross_validation import cross_val_score
-
-# Tf-Idf
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 import numpy as np
@@ -15,7 +13,6 @@ word_to_id = {}
 def parse_vocabulary(file):
     with open(file, 'rb') as f:
         vocab = pickle.load(f)
-
     count = 0
     for w in vocab:
         word_to_id[w] = count
@@ -41,43 +38,36 @@ def read_training_set(pos_tweets, neg_tweets, embeddings):
             features = np.array((extract_features(tweet, embeddings))).tolist()
             training_set.append(features)
             labels.append('positive')
-
     return training_set, labels
+#end
 
 #start get_word_vector
 def get_word_vector(word, embeddings):
     # Returns word vector from generated embeddings
     word_vector = np.array([])
-
     # If word exists in word_to_id dictionary, then grab the corresponding embeddings vector
     if word in word_to_id:
         embd_index = word_to_id[word]
         word_vector = embeddings[embd_index,:]
-
     return word_vector
 #end
 
 #start extract_features
 def extract_features(tweet, embeddings):
     tweet_words = set(tweet)
-
     features = np.array(embeddings.shape[0])
     word_count = 0
     for word in tweet_words:
         # get word from embeddings
         feature = get_word_vector(word, embeddings)
-
         if feature.size is 0: # If feature is empty, skip iteration
             continue
-
         # sum with others
         features = np.add(features, feature)
         # increase the count
         word_count += 1
-
     # divide by the word_count
     np.divide(features, word_count)
-
     return features
 #end
 
@@ -120,8 +110,8 @@ def main():
     embeddings = np.load('embeddings.npy')
 
     # Read the tweets and prepare them for classifier
-    pos_tweets = open('train_pos_cut.txt', 'r')
-    neg_tweets = open('train_neg_cut.txt', 'r')
+    pos_tweets = open('train_pos_10perc.txt', 'r')
+    neg_tweets = open('train_neg_10perc.txt', 'r')
     training_set, labels = read_training_set(pos_tweets, neg_tweets, embeddings)
 
     if tf_idf: # If Tf-Idf is used, vectorize the data we have
@@ -129,30 +119,20 @@ def main():
         training_set = vectorizer.fit_transform(training_set)
 
     # Split dataset for cross_validation
+    #training_set, test_set, training_set_labels, test_set_labels = cross_validation.train_test_split(training_set, labels, test_size=0.4, random_state=0)
 
-    """
-    training_set, test_set, training_set_labels, test_set_labels = cross_validation.train_test_split(
-        training_set, labels, test_size=0.4, random_state=0)
-    """
-
-    ###
     # Train SVM classifier & print the results
-    ###
-
-    classifier = svm.SVC()
-#    classifier.fit(training_set, training_set_labels)
-#    print_classified_with_tfidf(classifier, vectorizer) if tf_idf else print_classified(classifier, embeddings)
+    classifier = svm.LinearSVC()
+    #classifier.fit(training_set, training_set_labels)
+    #print_classified_with_tfidf(classifier, vectorizer) if tf_idf else print_classified(classifier, embeddings)
     scores_svm = cross_validate(classifier, training_set, labels)
     print("Accuracy of SVM %s" % scores_svm)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores_svm.mean(), scores_svm.std() * 2))
 
-    ###
     # Traing logistic regression classifier  & print the results
-    ###
-
     classifier = linear_model.LogisticRegression()
-#    classifier.fit(training_set, training_set_labels)
-#    print_classified_with_tfidf(classifier, vectorizer) if tf_idf else print_classified(classifier, embeddings)
+    #classifier.fit(training_set, training_set_labels)
+    #print_classified_with_tfidf(classifier, vectorizer) if tf_idf else print_classified(classifier, embeddings)
     scores_logReg = cross_validate(classifier, training_set, labels)
     print("Accuracy of log regression: %s" % scores_logReg)
     print("Acuracy: %0.2f (+/- %0.2f)" % (scores_logReg.mean(), scores_logReg.std() * 2))
