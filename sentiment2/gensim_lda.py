3a973d6f2a88e141e06a86bfe55c68ac1e8eca2f
pius <piusvd@gmail.com>
2016-06-21 22:54:56 +0200

    script to compute lda vectors

---
+++ sentiment2/gensim_lda.py
@@ -0,0 +1,64 @@
+
+import sys 
+
+import gensim
+
+from gensim.models.ldamodel import LdaModel
+
+import numpy as np
+
+from preprocess import MemoizedStemmer, load_pickled
+
+class TweetCorpus(gensim.corpora.textcorpus.TextCorpus):
+
+    def __init__(self, infile, vocab):
+        super(TweetCorpus, self).__init__()
+        self.infile = infile
+        self.stemmer = MemoizedStemmer()
+        self.vocab = vocab
+
+    def get_texts(self):
+        with open(self.infile, 'r', encoding='utf-8') as f:
+            for line in f:
+                stemmed = [self.stemmer.stem(w) for w in line.split()]
+                filter_vocab = [w for w in stemmed if self.vocab.get(w) is not None]
+                yield filter_vocab
+
+    def __iter__(self):
+        for txt in self.get_texts():
+            w_ids = [self.vocab[w] for w in txt]
+            yield list_to_bow(w_ids)
+
+def list_to_bow(word_ids):
+    d = {}
+
+    for w_id in word_ids:
+        if d.get(w_id) is None:
+            d[w_id] = 1
+        else:
+            d[w_id] += 1
+
+    return list(d.items())
+
+class LdaLoader(object):
+
+    def __init__(self, lda_file):
+        self.lda = LdaModel.load(lda_file)
+
+    def topic_vector(self, word_ids):
+        return self.lda[list_to_bow(word_ids)]
+
+def main(vocab_file, inv_vocab_file, infiles):
+    vocab = load_pickled(vocab_file)
+    inv_vocab = load_pickled(inv_vocab_file)
+
+    lda = LdaModel(id2word=inv_vocab, num_topics=200)
+
+    for f in infiles:
+        tc = TweetCorpus(f, vocab)
+        lda.update(tc)
+
+    lda.save('topics.lda')
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3:])
